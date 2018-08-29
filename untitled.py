e44d1147e82f76ef465353f700f95759f31a8816
hl <huanglei0814@foxmail.com>
2018-08-29 11:56:31 +0800

    自知则知之

--- untitled.py
+++ untitled.py
@@ -1,14 +1,21 @@
 from flask import Flask
-from flask_restful import Api, abort, reqparse, Resource
+from flask_restful import abort, reqparse, Resource, Api
+from peewee import *
 
 app = Flask(__name__)
 api = Api(app)
+
+@app.route('/')
+def hello_world():
+    return 'Hello World!'
 
 FOODS = {
     'foodid1': {'name': 'food 1', 'price': 24},
     'foodid2': {'name': 'food 2', 'price': 25},
     'foodid3': {'name': 'food 3', 'price': 26},
 }
+
+db = SqliteDatabase('food.db')
 
 def abort_if_food_doesnt_exist(food_id):
     if food_id not in FOODS:
@@ -17,6 +24,31 @@ def abort_if_food_doesnt_exist(food_id):
 parser = reqparse.RequestParser()
 parser.add_argument('name')
 parser.add_argument('price')
+
+class Dish(Model,Resource):
+    name = CharField()
+    price = DoubleField
+    def get(self,id):
+        return
+
+    def delete(self,food_id):
+        del FOODS[food_id]
+        return  '',204
+
+    def put(self,food_id):
+        args = parser.parse_args()
+        task = {'name':args['name'],'price':args['price']}
+        FOODS[food_id] = task
+        return task, 201
+
+
+    class Meta:
+        database = db # This model uses the "people.db" database.
+
+
+
+
+
 
 #Food
 #shows a single food item and lets you delete a food item
@@ -38,13 +70,22 @@ class Food(Resource):
 
 #FoodList
 # shows a list of all foods, and lets you POST to add new foods
-class TodoList(Resource):
+class FoodList(Resource):
     def get(self):
         return FOODS
 
     def post(self):
         args = parser.parse_args()
-        food_id = int(max(FOODS.keys()).lstrip('food')) + 1
-        food_id = 'food%i' % todo_id
-        TODOS[todo_id] = {'task': args['task']}
-        return TODOS[todo_id], 201
+        food_id = int(max(FOODS.keys()).lstrip('foodid')) + 1
+        food_id = 'foodid%i' % food_id
+        FOODS[food_id] = {'name':args['name'],'price':args['price']}
+        return FOODS[food_id], 201
+
+##
+## Actually setup the Api resource routing here
+##
+api.add_resource(FoodList, '/foods')
+api.add_resource(Food, '/foods/<food_id>')
+
+if __name__ == '__main__':
+    app.run()
